09dd0c7c1ebe00ce3fe3ee263c8ed0029e281402
Alexander Serheyev <alex@dna.tech>
2021-08-02 20:17:03 +0400

    typed.js integration

--- src/main.js
+++ src/main.js
@@ -4,9 +4,23 @@ import './styles/styles.scss'
 ///////////////////////////////////////////////////////////////////////////////
 // MAIN
 ///////////////////////////////////////////////////////////////////////////////
-
+import Typed from 'typed.js'
 const load = () => {
-  console.log('Hello, world!')
+  // var typed = new Typed('.typed', {
+  //   strings: ['First sentence.', 'Second sentence.'],
+  //   typeSpeed: 30
+  // })
+  var typed2 = new Typed('#printer', {
+    stringsElement: '#printer-string',
+    typeSpeed: 0,
+    backSpeed: 0,
+    fadeOut: true,
+    fadeOutDelay: 0,
+
+    // backDelay: 5000,
+    showCursor: true,
+    loop: true
+  })
 }
 
 document.addEventListener('DOMContentLoaded', load)
