a5fc078a7d4dd11f83f0c00cc39a3fc157ebd2e4
Alexander <alex@dna.tech>
2021-08-09 23:05:26 +0400

    Remove intro

--- src/main.js
+++ src/main.js
@@ -2,23 +2,11 @@ import './styles/styles.scss'
 import Typed from 'typed.js'
 
 const load = () => {
-  if (window.innerWidth > 380) {
-    const intro = new Typed('#printer-intro', {
-      stringsElement: '#intro',
-      typeSpeed: 0,
-      fadeOut: true,
-      fadeOutDelay: 20,
-      loop: false,
-      showCursor: false,
-      cursorChar: ''
-    })
-  }
-
   const strings = new Typed('#printer-strings', {
     stringsElement: '#strings',
     startDelay: 2000,
     smartBackspace: true,
-    typeSpeed: 0,
+    typeSpeed: 30,
     backDelay: 1000,
     loop: true,
     showCursor: true,
