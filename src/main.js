fe408929f2cc99f41db2d0b10591862741d07ed8
Alexander Serheyev <alex@dna.tech>
2021-08-09 14:25:23 +0400

    Fix small screens

--- src/main.js
+++ src/main.js
@@ -2,15 +2,17 @@ import './styles/styles.scss'
 import Typed from 'typed.js'
 
 const load = () => {
-  const intro = new Typed('#printer-intro', {
-    stringsElement: '#intro',
-    typeSpeed: 0,
-    fadeOut: true,
-    fadeOutDelay: 20,
-    loop: false,
-    showCursor: false,
-    cursorChar: ''
-  })
+  if (window.innerWidth > 380) {
+    const intro = new Typed('#printer-intro', {
+      stringsElement: '#intro',
+      typeSpeed: 0,
+      fadeOut: true,
+      fadeOutDelay: 20,
+      loop: false,
+      showCursor: false,
+      cursorChar: ''
+    })
+  }
 
   const strings = new Typed('#printer-strings', {
     stringsElement: '#strings',
